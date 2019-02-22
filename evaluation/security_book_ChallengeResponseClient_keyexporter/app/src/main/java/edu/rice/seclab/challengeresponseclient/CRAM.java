0ecb6cc02a76ec3b3b3deab2c1dc1ae3b72301ee
Jaeho Lee <Jaeho.Lee@rice.edu>
2019-02-22 00:26:18 -0600

    add evluation

---
+++ evaluation/security_book_ChallengeResponseClient_keyexporter/app/src/main/java/edu/rice/seclab/challengeresponseclient/CRAM.java
@@ -0,0 +1,58 @@
+package edu.rice.seclab.challengeresponseclient;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import android.app.Activity;
+import android.util.Base64;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import edu.rice.seclab.keyexporter.HMacKeyExporter;
+
+public class CRAM {
+    private Activity activity;
+
+    public CRAM(Activity act) {
+        activity = act;
+    }
+
+    public String generate(String serverChallenge) {
+        String algo = "HmacSHA1";
+        TextView pass = (TextView) activity.findViewById(R.id.editText2);
+        byte[] server = Base64.decode(serverChallenge, Base64.DEFAULT);
+
+        HMacKeyExporter auth = new HMacKeyExporter("HmacSHA1");
+        auth.bind((EditText)pass);
+
+//        Mac mac = null;
+//        try {
+//            mac = Mac.getInstance(algo);
+//        } catch (NoSuchAlgorithmException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        String keyText = pass.getText().toString();
+//        SecretKey key = new SecretKeySpec(keyText.getBytes(), algo);
+//        try {
+//            mac.init(key);
+//        } catch (InvalidKeyException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        byte[] tmpHash = mac.doFinal(server);
+
+        auth.init();
+        auth.update(server);
+        byte [] tmpHash = auth.getKeyBytes();
+
+        TextView user = (TextView) activity.findViewById(R.id.editText1);
+        String username = user.getText().toString();
+        String concat = username + " " + Hex.toHex(tmpHash);
+        String hash = Base64.encodeToString(concat.getBytes(), Base64.URL_SAFE);
+        return hash;
+    }
+}
